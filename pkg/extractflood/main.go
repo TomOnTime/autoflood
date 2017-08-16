f626e2707e011e73564336984fbfbb6694e1be6e
Tom Limoncelli <tal@whatexit.org>
2017-08-15 23:45:23 -0400

    wip

--- pkg/extractflood/main.go
+++ pkg/extractflood/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/pkg/errors"
 
 	// Uncomment the decoders you want to activate.
+	"image/color"
 	_ "image/png"
 	// _ "image/gif"
 	// _ "image/jpeg"
@@ -21,8 +22,8 @@ func ExtractFile(filename string) error {
 	// Other phone sizes may result in different dimensions.
 	//minX, minY := 14, 278
 	//maxX, maxY := 625, 1047
-	minX, minY := 40, 207
-	maxX, maxY := minX+559, minY+561
+	var minX, minY int = 40, 207
+	var maxX, maxY int = minX + 559, minY + 561
 
 	reader, err := os.Open(filename)
 	if err != nil {
@@ -72,46 +73,62 @@ func ExtractFile(filename string) error {
 		}
 	}
 	var size string
+	var sz int
 	fmt.Println("table")
 	for irow, row := range runtable {
-		//		if row != 0 {
-		//			fmt.Println(irow, row)
-		//		}
-		if row == 0 {
+		if irow == 0 {
 			continue
 		}
-		if row > 10 && row > 1000 {
+		if row > 1000 {
 			fmt.Println(irow, row)
 			switch irow {
 			case 24:
 				size = "LARGE"
+				sz = 24
 			case 32, 64:
 				size = "MEDIUM"
+				sz = 32
 			case 48:
 				size = "SMALL"
+				sz = 48
 			default:
 			}
 		}
 	}
 
 	fmt.Printf("size: %s\n", size)
-	return nil
 
-	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-		if y < minY || y > maxY {
-			continue
+	for y := minY + (sz / 2); y < maxY; y = y + sz {
+		fmt.Printf("r %04d", y)
+		for x := minX + (sz / 2); x < maxX; x = x + sz {
+			//r, g, b, a := m.At(x, y).RGBA()
+			//fmt.Printf(" %03d:%03d:%03d:%03x", r>>8, g>>8, b>>8, a>>8)
+			c := vote(m.At(x, y), m.At(x+1, y), m.At(x, y+1))
+			fmt.Printf(" %s", letter(c))
 		}
-		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			if x < minX || x > maxX {
-				continue
-			}
-			if x%44 != 0 {
-				continue
-			}
-			r, g, b, a := m.At(x, y).RGBA()
-			fmt.Printf("pixel %04d:%04d %02d %02d %02d %02x\n", x, y, r>>8, g>>8, b>>8, a)
-		}
+		fmt.Println()
 	}
 
 	return nil
+}
+
+func vote(a, b, c color.Color) color.Color {
+	if a == b && b == c {
+		return a
+	}
+	if a == b {
+		return a
+	}
+	if a == c {
+		return a
+	}
+	if b == c {
+		return b
+	}
+	fmt.Printf("warning: no majority %v %v %v\n", a, b, c)
+	return a
+}
+
+func letter(c color.Color) string {
+	return "c"
 }
