314925733d70d01bf9166570ef81767a99825005
Tom Limoncelli <tal@whatexit.org>
2017-08-20 21:50:51 -0400

    Detects grid

--- pkg/extractflood/main.go
+++ pkg/extractflood/main.go
@@ -23,7 +23,9 @@ func ExtractFile(filename string) error {
 	//minX, minY := 14, 278
 	//maxX, maxY := 625, 1047
 	var minX, minY int = 40, 207
-	var maxX, maxY int = minX + 559, minY + 561
+	var maxX, maxY int = minX + 561, minY + 561
+	var lenX = maxX - minX
+	var lenY = maxY - minY
 
 	reader, err := os.Open(filename)
 	if err != nil {
@@ -36,6 +38,7 @@ func ExtractFile(filename string) error {
 		log.Fatal(err)
 	}
 	bounds := m.Bounds()
+	bmy := bounds.Max.Y
 	// TODO(tlim): Error if bounds != (0,0)-(640,1136)
 	fmt.Println(bounds)
 
@@ -54,7 +57,7 @@ func ExtractFile(filename string) error {
 			if x < minX || x > maxX {
 				continue
 			}
-			r, g, b, a := m.At(x, y).RGBA()
+			r, g, b, a := m.At(x, bmy-y).RGBA()
 			//fmt.Printf("pixel %04x:%04d %02d %02d %02d %02x\n", x, y, r, g, b, a)
 			if first {
 				first = false
@@ -72,6 +75,12 @@ func ExtractFile(filename string) error {
 			}
 		}
 	}
+
+	// style rows   pixels    total
+	// small:   12   46.75    561
+	// medium   17   33       561
+	// large    22   25.5     561
+
 	var size string
 	var sz int
 	fmt.Println("table")
@@ -84,29 +93,40 @@ func ExtractFile(filename string) error {
 			switch irow {
 			case 24:
 				size = "LARGE"
-				sz = 24
+				sz = 22
 			case 32, 64:
 				size = "MEDIUM"
-				sz = 32
+				sz = 17
 			case 48:
 				size = "SMALL"
-				sz = 48
+				sz = 12
 			default:
 			}
 		}
 	}
+	fmt.Printf("size=%s\n", size)
+	widthX := lenX / sz
+	widthY := lenY / sz
+	fmt.Printf("widthX=%d widthY=%d\n", widthX, widthY)
 
-	fmt.Printf("size: %s\n", size)
-
-	for y := minY + (sz / 2); y < maxY; y = y + sz {
-		fmt.Printf("r %04d", y)
-		for x := minX + (sz / 2); x < maxX; x = x + sz {
-			//r, g, b, a := m.At(x, y).RGBA()
-			//fmt.Printf(" %03d:%03d:%03d:%03x", r>>8, g>>8, b>>8, a>>8)
-			cl := []color.Color{m.At(x, y), m.At(x+1, y), m.At(x, y+1), m.At(x+1, y+1), m.At(x+1, y-1)}
+	for y := sz - 1; y >= 0; y-- {
+		for x := 0; x < sz; x++ {
+			// pixel start of the square:
+			px := minX + (lenX * x / sz) // more accurate than (lenX/sz)*x + minX
+			py := minY + (lenY * y / sz) // more accurate than (lenY/sz)*y + minY
+			// Mid-point
+			mx := px + (widthX / 2)
+			my := py + (widthY / 2)
+			cl := []color.Color{
+				m.At(mx-1, bmy-(my-1)), m.At(mx, bmy-(my-1)), m.At(mx+1, bmy-(my-1)),
+				m.At(mx-1, bmy-(my)), m.At(mx, bmy-(my)), m.At(mx+1, bmy-(my)),
+				m.At(mx-1, bmy-(my+1)), m.At(mx, bmy-(my+1)), m.At(mx+1, bmy-(my+1)),
+			}
 			c, err := vote(cl)
+			//fmt.Printf("%2d:%-2d %3d:%3d %3d:%3d %s [[%v]]\n", x, y, px, py, mx, my, letter(c), c)
+			//fmt.Printf("                         %v\n", cl)
 			if err != nil {
-				fmt.Printf("ERROR: %s\n", err)
+				fmt.Printf("\nERROR: %s (c=%v)\n", err, c)
 			}
 			fmt.Printf(" %s", letter(c))
 		}
@@ -117,19 +137,15 @@ func ExtractFile(filename string) error {
 }
 
 func vote(cl []color.Color) (color.Color, error) {
-
 	var err error
-
 	tally := map[string]int{}
-	//orig := map[string]color.Color{}
 
 	var max int
 	var maxc color.Color
 
 	for _, c := range cl {
 		r, g, b, _ := c.RGBA()
-		u := fmt.Sprintf("%04x%04x%04x", r, g, b)
-		//orig[u] = c
+		u := fmt.Sprintf("%04x%04x%04x", r>>12, g>>12, b>>12)
 		tally[u]++
 		if tally[u] > max {
 			max = tally[u]
@@ -145,12 +161,28 @@ func vote(cl []color.Color) (color.Color, error) {
 	}
 
 	if cm != 1 {
-		err = fmt.Errorf("no majority in %v\n", cl)
+		err = fmt.Errorf("no majority in %v ==== %v\n", cl, tally)
 	}
 
 	return maxc, err
 }
 
+var color2letter = map[string]string{}
+var lastletter int
+var letters = "ABCDEF"
+
 func letter(c color.Color) string {
-	return "c"
+	r, g, b, _ := c.RGBA()
+	u := fmt.Sprintf("%04x%04x%04x", r>>12, g>>12, b>>12)
+	v, ok := color2letter[u]
+	if ok {
+		return v
+	}
+	newletter := letters[lastletter : lastletter+1]
+	lastletter++
+	color2letter[u] = newletter
+	return newletter
+
+	//r, g, b, _ := c.RGBA()
+	//return fmt.Sprintf(" (%d,%d,%d)", r, g, b)
 }
