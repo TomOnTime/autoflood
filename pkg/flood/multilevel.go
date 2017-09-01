baef82f8147373f41ebce757a1e1c1112a032cd4
Tom Limoncelli <tlimoncelli@stackoverflow.com>
2017-08-31 23:39:09 -0400

    WIP

--- pkg/flood/multilevel.go
+++ pkg/flood/multilevel.go
@@ -1,12 +1,23 @@
 package flood
 
+// NEXT: The score should be more than a simple count
+
 import (
 	"container/heap"
 	"fmt"
 )
 
 // Search for the best next move.
-func (g *Game) SearchMultiLevel() Buttons {
+func (g *Game) SearchMultiLevel(moveSoFar int) Buttons {
+
+	tries := 20000
+
+	// start out our "best" as just beyond optimal.
+	bestSoFar := &Item{
+		path: make(ButtonPath, 999),
+		//path:  make(ButtonPath, g.MaxMoves+1),
+		score: 2, // TODO: Try score: 0
+	}
 
 	// Initialize the priority queue
 	pq := make(PriorityQueue, 0)
@@ -20,10 +31,15 @@ func (g *Game) SearchMultiLevel() Buttons {
 		// pick best score so far.
 		bestItem := heap.Pop(&pq).(*Item)
 
-		// if max depth: return this {path,score}
-		if len(bestItem.path) == g.MaxMoves {
-			return bestItem.path[0]
-			// BUG???
+		// If the path we're about to try is equal or longer
+		// than our current best, skip it.
+		//  best +1 +2     bestSoFar
+		//   17  18 19     19  try it
+		//   18  19 20     19  skip
+		//   19  20 21     19  skip
+		//   20  21 22     19  skip
+		if (len(bestItem.path) + 2) > len(bestSoFar.path) {
+			continue
 		}
 
 		// generate a {past + button, score} for each button (remove any loss)
@@ -44,17 +60,34 @@ func (g *Game) SearchMultiLevel() Buttons {
 				score: nextScore,
 				state: &nextState,
 			}
-			fmt.Printf("(%d) Path %s would score %d\n", len(pq), newItem.path, newItem.score)
-			if nextScore == g.WinScore {
-				fmt.Printf("RETURN first of %s\n", newItem.path)
-				return newItem.path[0]
-			}
-			//fmt.Printf("PUSHING %v %v %s\n", newItem.path, newItem.score, *newItem.state)
-			heap.Push(&pq, newItem)
+			fmt.Printf("(%3d) Path %s would score %d (this=%d best=%d)\n", len(pq), newItem.path, newItem.score, len(newItem.path), len(bestSoFar.path))
 
+			if nextScore == g.WinScore {
+				// Winning path? Record if it beats our best so far.
+				if len(newItem.path) < len(bestSoFar.path) {
+					bestSoFar = bestItem
+				}
+			} else if len(newItem.path) == g.MaxMoves {
+				// No more moves? Skip.
+				continue
+			} else if (bestSoFar.score != g.WinScore) && nextScore > bestSoFar.score {
+				// If we don't yet have a winning score, and this is better
+				// than what we have, record it as best so far.
+				// bestSoFar = newItem
+			}
+			// Record it for future analysis:
+			heap.Push(&pq, newItem)
+			//fmt.Printf("PUSHING %v %v %s\n", newItem.path, newItem.score, *newItem.state)
+
+		}
+
+		tries--
+		if tries == 0 {
+			break
 		}
 	}
 
+	return bestSoFar.path[0]
 }
 
 // try attempts a button press without modifying the game.
